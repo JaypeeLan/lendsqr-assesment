3acefd149df7ed26eef861483af7a9b2f3ef379d
Laniran JohnPaul <jplaniran01@gmail.com>
2024-05-29 16:56:07 +0100

    sidebar links and icons

--- components/DashboardSidebar/Sidebar.tsx
+++ components/DashboardSidebar/Sidebar.tsx
@@ -1,12 +1,16 @@
+"use client";
+import classNames from "classnames";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { BUSINESS, CUSTOMERS, SETTINGS } from "./links";
 
 interface LinksCardProps {
-  link: string;
+  link?: string;
   name: string;
   rightIcon?: string;
   activeLink?: boolean;
+  selectedLink?: string;
 }
 
 const LinksCard = ({
@@ -14,11 +18,24 @@ const LinksCard = ({
   name,
   rightIcon,
   activeLink = false,
+  selectedLink,
 }: LinksCardProps) => {
   return (
-    <Link href={`/dashboard/${link}`} className="dashboard-link">
+    <Link
+      href={`/dashboard/${link}`}
+      className={classNames("dashboard-link", { selectedLink })}
+    >
       {/* leftIcon */}
-      <Image src={`/icons/${link}.svg`} alt={name} width={16} height={16} />
+      <Image
+        src={
+          link
+            ? `/dashboardIcons/${link}.svg`
+            : `/dashboardIcons/${name.toLowerCase()}.svg`
+        }
+        alt={name}
+        width={16}
+        height={16}
+      />
       <p className={activeLink ? "activeLink" : ""}>{name}</p>
       {rightIcon && (
         <Image
@@ -33,20 +50,34 @@ const LinksCard = ({
 };
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="sidebar">
       <LinksCard
         name="Switch Organization"
         link="switch-organization"
         rightIcon="dropdown-arrow"
+        activeLink
+        selectedLink={
+          pathname === "/dashboard/switch-organization" ? "selectedLink" : ""
+        }
       />
-      <LinksCard name="Dashboard" link="dashboard" activeLink />
+      <LinksCard
+        name="Dashboard"
+        activeLink={pathname === "/dashboard"}
+        selectedLink={pathname === "/dashboard" ? "selectedLink" : ""}
+      />
       <p className="sidebar-title">customers</p>
       {CUSTOMERS.map((customerLink) => (
         <LinksCard
           key={customerLink.url}
           name={customerLink.name}
           link={customerLink.url}
+          activeLink={pathname === `/dashboard/${customerLink.url}`}
+          selectedLink={
+            pathname === `/dashboard/${customerLink.url}` ? "selectedLink" : ""
+          }
         />
       ))}
       <p className="sidebar-title">business</p>
@@ -55,6 +86,10 @@ const Sidebar = () => {
           key={businessLink.url}
           name={businessLink.name}
           link={businessLink.url}
+          activeLink={pathname === `/dashboard/${businessLink.url}`}
+          selectedLink={
+            pathname === `/dashboard/${businessLink.url}` ? "selectedLink" : ""
+          }
         />
       ))}
       <p className="sidebar-title">settings</p>
@@ -63,6 +98,10 @@ const Sidebar = () => {
           key={settingsLink.url}
           name={settingsLink.name}
           link={settingsLink.url}
+          activeLink={pathname === `/dashboard/${settingsLink.url}`}
+          selectedLink={
+            pathname === `/dashboard/${settingsLink.url}` ? "selectedLink" : ""
+          }
         />
       ))}
     </div>
